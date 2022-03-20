1e7d2e573ea2955c0ad452139bb6ab2a60d96fbe
simsve3 <simsve3@ktu.lt>
2022-03-20 02:37:31 +0200

    Done login

--- projektas_front/src/Pages/Login.js
+++ projektas_front/src/Pages/Login.js
@@ -3,17 +3,19 @@ import '../App.css';
 import { TextField } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Email } from '@material-ui/icons';
 
 function App() {
   const navigate = useNavigate();
+
   return (
     <div className="App">
       <header className="App-header">
        <h1>Login</h1>
        <TextField
           required
-          id="outlined-required"
-          label="Username or e-mail"
+          id="email"
+          label="E-mail"
           InputLabelProps={{
             style: { color: 'white'},
           }}
@@ -21,19 +23,42 @@ function App() {
         <br></br>
         <TextField
             required
-            id="outlined-required"
+            id="password"
             label="Password"
             type="password"
             InputLabelProps={{
               style: { color: 'white'},
             }}
           />
-        <Button variant="outlined" onClick={() => {navigate('/deposit')}}>Login</Button>
+          <br></br>
+          <label id="error"></label>
+        <Button variant="outlined" onClick={CheckInfo}>Login</Button>
         <br></br>
         <Button variant="outlined" onClick={() => {navigate('/registration')}}>Registration</Button>
       </header>
     </div>
   );
+}
+
+function CheckInfo(){
+  if (localStorage && 'Email' in localStorage) {
+    var Email = localStorage.getItem("Email");
+  }
+  if (localStorage && 'Password' in localStorage) {
+    var Password = localStorage.getItem("Password");
+  }
+
+  var email=document.getElementById("email").value;
+  var password=document.getElementById("password").value;
+
+  if(email==Email && password==Password)
+  {
+    window.location.href='/deposit';
+  }
+  else
+  {
+    document.getElementById('error').innerHTML = 'Login information is incorrect';
+  }
 }
 
 export default App;
--- projektas_front/src/Pages/Registration.js
+++ projektas_front/src/Pages/Registration.js
@@ -3,54 +3,86 @@ import '../App.css';
 import { TextField } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
 
 function App() {
   const navigate = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Registration</h1>
-       <TextField
-          required
-          id="outlined-required"
-          label="Username"
-          InputLabelProps={{
-            style: { color: 'white'},
-          }}
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="E-mail"
-          InputLabelProps={{
-            style: { color: 'white'},
-          }}
-        />
-        <TextField
+    <form>
+      <div className="App">
+        <header className="App-header">
+          <h1>Registration</h1>
+          <TextField
+            inputProps={{ inputMode: 'email'}}
             required
-            id="outlined-required"
-            label="Password"
-            type="password"
+            id="email"
+            label="E-mail"
+            type="email"
             InputLabelProps={{
               style: { color: 'white'},
             }}
           />
           <TextField
-            required
-            id="outlined-required"
-            label="Re-enter password"
-            type="password"
-            InputLabelProps={{
-              style: { color: 'white'},
-            }}
+              inputProps={{ inputMode: 'text'}}
+              required
+              id="password"
+              label="Password"
+              type="password"
+              InputLabelProps={{
+                style: { color: 'white'},
+              }}
+            />
+          <TextField
+              inputProps={{ inputMode: 'text'}}
+              required
+              id="repassword"
+              label="Re-enter password"
+              type="password"
+              InputLabelProps={{
+                style: { color: 'white'},
+              }}
           />
-        <Button variant="outlined" onClick={() => {navigate('/')}}>Sign-up</Button>
-
-        <Button variant="outlined" onClick={() => {navigate('/')}}>Back</Button>
-        <br></br>
-      </header>
-    </div>
+          <br></br>
+          <label id="error"></label>
+          <label id="succesful"></label>
+          <Button variant="outlined" onClick={SaveValues}>Sign-up</Button>
+          <Button variant="outlined" onClick={() => {navigate('/')}}>Back</Button>
+          <br></br>
+        </header>
+      </div>
+    </form>
   );
+}
+
+function SaveValues(){
+  var email=document.getElementById("email").value;
+  var password=document.getElementById("password").value;
+  var Repassword=document.getElementById("repassword").value;
+  document.getElementById('error').innerHTML = '';
+  document.getElementById('succesful').innerHTML = '';
+  if(email==localStorage.getItem("Email"))
+  {
+    document.getElementById('error').innerHTML = 'This user is already registered';
+  }
+  else if(!email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
+  {
+    document.getElementById('error').innerHTML = 'Email is not in a correct format';
+  }
+  else if(!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/))
+  {
+    document.getElementById('error').innerHTML = 'Password is not in the correct format. There has to be at least one uppercase letter, lowercase letter, and number';
+  }
+  else if(password!=Repassword)
+  {
+    document.getElementById('error').innerHTML = 'Passwords do not match';
+  }
+  else
+  {
+    document.getElementById('succesful').innerHTML = 'Succesfully registered';
+    localStorage.setItem("Email",email)
+    localStorage.setItem("Password",password);
+  }
 }
 
 export default App;
